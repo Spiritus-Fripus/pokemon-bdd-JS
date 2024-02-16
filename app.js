a1a4cc0b26ef78691a51e19443a45df0b0464979
Spiritus-Fripus <N-weber@live.fr>
2024-02-16 22:37:41 +0100

    changes

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-'use strict'
+"use strict";
 
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
 
-fetch('http://localhost/projet_get/getData.php')
-  .then(response => response.json())
-  .then(data => {
+fetch("http://localhost/projet_get/getData.php")
+  .then((response) => response.json())
+  .then((data) => {
     const pokemons = data.slice();
 
-    pokemons.map(pokemon => {
-      console.log(pokemon)
-      container.insertAdjacentHTML('beforeend', `
+    pokemons.map((pokemon) => {
+      console.log(pokemon);
+      container.insertAdjacentHTML(
+        "beforeend",
+        `
     <div class="pokemon-card ${pokemon.typ_libelle}">
       <div class="pokemon-name">
       <h2>${pokemon.pok_nom}</h2>
@@ -22,9 +24,10 @@ fetch('http://localhost/projet_get/getData.php')
         <span>Poids: ${pokemon.pok_poids} kg | Taille: ${pokemon.pok_taille} <br> Type: ${pokemon.typ_libelle} | N°: ${pokemon.pok_id}  </span>
       </div>
     </div>
-    `)
-    })
+    `
+      );
+    });
   })
-  .catch(error => {
-    console.error('Oupsi y à une erreur:', error);
+  .catch((error) => {
+    console.error("Oupsi y à une erreur:", error);
   });
